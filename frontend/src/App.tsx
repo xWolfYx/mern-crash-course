0d4ab1ed8201619e5e21e9d4c2aeb688df1600fe
xWolfYx <david.skliarov.dev@gmail.com>
2026-02-21 17:31:26 +0400

    feat: add app.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router";
+import CreatePage from "./pages/createPage";
+import HomePage from "./pages/homepage";
+import Navbar from "./components/navbar";
+import { useColorModeValue } from "./components/ui/color-mode";
+
+function App() {
+  return (
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
