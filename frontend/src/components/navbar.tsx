106b6ef5edf9ef372030e4bebb2c3c02a0b20207
xWolfYx <david.skliarov.dev@gmail.com>
2026-02-21 17:30:54 +0400

    feat: add navbar

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,43 @@
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { BsPlusSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useColorMode } from "./ui/color-mode";
+import { FaRegMoon, FaRegSun } from "react-icons/fa";
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="to-r"
+          gradientFrom="cyan.400"
+          gradientTo="blue.500"
+          bgClip={"text"}
+        >
+          <Link to="/">Product Store</Link>
+        </Text>
+        <HStack gap={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button variant={"subtle"}>
+              <BsPlusSquare />
+            </Button>
+          </Link>
+          <Button variant={"subtle"} onClick={toggleColorMode}>
+            {colorMode === "light" ? <FaRegSun /> : <FaRegMoon />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+}
