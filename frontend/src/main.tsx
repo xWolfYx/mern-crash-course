c063e5957035c68e8250c77a3ec7c718dde9e0c9
xWolfYx <david.skliarov.dev@gmail.com>
2026-02-21 17:31:16 +0400

    feat: add main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "./components/ui/provider.tsx";
+import { BrowserRouter } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
+);
