2a604cf4deaf315d23d5ad46411aae6a2a962879
xWolfYx <david.skliarov.dev@gmail.com>
2026-02-21 13:47:52 +0400

    fix: backend not connecting with the database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db.js";
+import dns from "node:dns/promises";
+
+import productRoutes from "./routes/product.route.js";
+
+dns.setServers(["1.1.1.1", "8.8.8.8", "1.0.0.1", "8.8.4.4"]);
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
 
-app.get("/products", (req, res) => {
-	res.send("Server is ready");
-});
+app.use(express.json()); // allows to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 console.log(process.env.MONGO_URI);
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log(`Server started at http://localhost:${PORT}`);
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
 });
