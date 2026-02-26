7b62d01f8cdbc1e41b05c684b6e746adff05753c
WolfY <david.skliarov.dev@gmail.com>
2026-02-26 15:47:02 +0400

    feat: add server config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
 });
